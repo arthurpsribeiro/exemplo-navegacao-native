8673667d1bdeea2f6a0e37a6a4a562112fd9ef91
arthurpsribeiro <arthurpsribeiro@gmail.com>
2021-06-24 15:30:59 -0300

    menu hamberguer

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import Constants from "expo-constants";
+import Header from "../components/Header";
 
 export default (props) => {
 	return (
 		<View style={styles.container}>
+			<Header navigation={props.navigation} />
 			<Text>Home</Text>
 			<Button
-				title="Ir para Produto"
-				onPress={() => {
-					props.navigation.navigate("Product");
-				}}
+				title="Ir para produto"
+				onPress={() => props.navigation.navigate("Produto")}
 			/>
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		flex: 1,
+		paddingTop: Constants.statusBarHeight,
+	},
 });
